c67c23d6b22e063b9a17be42d463101283cc2572
furkanavc1 <furkanavci039@gmail.com>
2024-11-12 20:11:07 +0300

    settings page buttons, credits added

---
+++ components/Modals/Credits.vue
@@ -0,0 +1,74 @@
+<script setup>
+const isOpen = defineModel("isOpen", { type: Boolean });
+</script>
+
+<template>
+  <UModal
+    v-model="isOpen"
+    :ui="{
+      container: 'items-center !shadow-none',
+      width: 'w-full',
+      background: '!bg-transparent',
+      shadow: '!shadow-none',
+      inner: 'hide-scrollbar',
+    }"
+    fullscreen
+  >
+    <div class="hide-scrollbar relative w-full max-w-xl">
+      <UIcon
+        @click="isOpen = false"
+        name="material-symbols-light:close-rounded"
+        size="24"
+        class="absolute right-5 top-5 z-20 cursor-pointer"
+      />
+      <div
+        class="hide-scrollbar relative animate-scrollCredits select-none space-y-6 py-44 text-center !text-white ease-linear"
+      >
+        <div>
+          <h2 class="text-2xl font-semibold">CO CEO & CTO</h2>
+          <p class="text-lg">Faruk ARIGUN</p>
+        </div>
+        <div>
+          <h2 class="text-2xl font-semibold">CO CEO</h2>
+          <ul class="list-none space-y-1">
+            <li>Umurcan GORUR</li>
+          </ul>
+        </div>
+        <div>
+          <h2 class="text-2xl font-semibold">Lead Developer</h2>
+          <ul class="list-none space-y-1">
+            <li>Batihan OZDEMIR</li>
+          </ul>
+        </div>
+        <div>
+          <h2 class="text-2xl font-semibold">Frontend Developers</h2>
+          <ul class="list-none space-y-1">
+            <li>Furkan AVCI</li>
+          </ul>
+        </div>
+        <div>
+          <h2 class="text-2xl font-semibold">Backend Developers</h2>
+          <ul class="list-none space-y-1">
+            <li>Erol Umut ATALAY</li>
+            <li>Ugur Can CELIKEL</li>
+          </ul>
+        </div>
+        <div>
+          <h2 class="text-2xl font-semibold">Head of Design</h2>
+          <p class="text-lg">Atilla GUNES</p>
+        </div>
+      </div>
+    </div>
+  </UModal>
+</template>
+
+<style scoped>
+@keyframes scrollCredits {
+  0% {
+    transform: translateY(100%);
+  }
+  100% {
+    transform: translateY(-100%);
+  }
+}
+</style>
--- components/Settings/index.vue
+++ components/Settings/index.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
-const emit = defineEmits(["mounted"]);
-onMounted(() => {
-  nextTick(() => {
-    emit("mounted");
-  });
-});
+const credistModal = ref(false);
+const startCredits = () => {
+  credistModal.value = !credistModal.value;
+};
 </script>
 <template>
-  <div>sett</div>
+  <div class="flex w-full flex-col gap-2 px-5 pt-5">
+    <ModalsCredits v-model:is-open="credistModal" />
+
+    <h1 class="text-[#D4D4D4]">Settings</h1>
+    <button class="settings-button h-10">Tutorial</button>
+    <button class="settings-button h-10" @click="startCredits()">
+      Credits
+    </button>
+    <button class="settings-button h-10">Watch Story</button>
+  </div>
 </template>
+<style>
+.settings-button {
+  border-radius: 6px;
+  border: 1px solid #702f9b;
+  background: radial-gradient(
+    169.72% 89.02% at 80.91% -10.53%,
+    rgba(175, 119, 184, 0.5) 0%,
+    rgba(66, 0, 124, 0.5) 100%
+  );
+  backdrop-filter: blur(10px);
+}
+</style>
