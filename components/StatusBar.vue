dc37b81b5345f029b3d3345209e4e5d13a766156
furkanavc1 <furkanavci039@gmail.com>
2024-11-13 21:32:27 +0300

    $ TO SP

--- components/StatusBar.vue
+++ components/StatusBar.vue
@@ -32,7 +32,7 @@ console.log(socialPoints.value);
           <div
             class="absolute left-0 flex size-8 items-center justify-center rounded-full border border-social-blue-200 bg-social-yellow-100"
           >
-            <span class="font-semibold text-social-blue-200">$</span>
+            <span class="font-semibold text-social-blue-200">SP</span>
           </div>
         </div>
         <span class="px-[1px] text-sm font-semibold text-social-blue-200"
