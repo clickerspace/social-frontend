97a1ef77155188736835a556c8c5d4932b32cc88
furkanavc1 <furkanavci039@gmail.com>
2024-11-13 22:07:46 +0300

    wallet clkr token card

--- components/Wallet/index.vue
+++ components/Wallet/index.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
 const emit = defineEmits(["update:key"]);
+import { userStore } from "~/store/user";
+import { storeToRefs } from "#build/imports";
+
+const { cryptoToken } = storeToRefs(userStore());
 </script>
 <template>
   <PhoneLayout title="Wallet" @update:key="emit('update:key')">
     <WalletState />
+    <div class="radial-bg flex flex-col gap-1 p-2">
+      <span class="text-sm font-semibold">Your $CLKR</span>
+      <div class="flex items-center">
+        <img src="/icons/clicker.svg" alt="clicker" class="size-8" />
+        <span>{{ cryptoToken ?? "0" }}</span>
+      </div>
+    </div>
   </PhoneLayout>
 </template>
