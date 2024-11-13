15975d9ae96af7ec3f5dc83c3f14185d182dbc72
furkanavc1 <furkanavci039@gmail.com>
2024-11-13 17:25:08 +0300

    task card style

--- components/Tasks/index.vue
+++ components/Tasks/index.vue
@@ -2,5 +2,7 @@
 const emit = defineEmits(["update:key"]);
 </script>
 <template>
-  <PhoneLayout title="Tasks" @update:key="emit('update:key')">task</PhoneLayout>
+  <PhoneLayout title="Tasks" @update:key="emit('update:key')">
+    <TasksCard v-for="item in 5" :key="item" />
+  </PhoneLayout>
 </template>
