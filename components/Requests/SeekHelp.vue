4fb431fd3ee0205607c7c502bb62c7684872e251
gokhancaliskan <caliskancompany@gmail.com>
2024-11-15 16:33:29 +0300

    styling for SeekHelp component with improved text and button design

--- components/Requests/SeekHelp.vue
+++ components/Requests/SeekHelp.vue
@@ -3,7 +3,9 @@ import { modalStore } from "~/store/modalStore";
 </script>
 <template>
   <div class="radial-bg flex w-full flex-col p-3">
-    <div class="flex w-full flex-col gap-3 text-white dark:text-white">
+    <div
+      class="flex w-full flex-col gap-3 text-sm font-semibold leading-[normal] text-white dark:text-white"
+    >
       <div class="flex w-full items-center justify-between">
         <span class="w-28 whitespace-nowrap"> Need a Hand?</span>
         <UIcon
@@ -13,7 +15,7 @@ import { modalStore } from "~/store/modalStore";
         />
       </div>
       <button
-        class="flex w-full items-center justify-center rounded-md border border-white bg-[#BDBDBD] px-2 py-1 dark:border-white"
+        class="flex w-full items-center justify-center rounded-md border-[1px] border-[#BDBDBD] border-[solid] bg-[radial-gradient(169.72%_89.02%_at_80.91%_-10.53%,_rgba(255,_255,_255,_0.50)_0%,_rgba(175,_175,_175,_0.50)_100%)] px-2 py-1 py-2 font-semibold"
         @click="modalStore().changeActiveKey('contacts')"
       >
         Request Help
